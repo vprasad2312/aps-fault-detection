defbbcf1a5127523fc60508cfe68cb5d6d87d4a1
vprasad2312 <vprasad2312@gmail.com>
2023-10-06 21:56:39 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Vivek',
+    author_email='vprasad2312@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
